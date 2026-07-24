daf8aedcc46849b954348f7a196d89dd33b48e6a
Jean Paul Michel <wake211856@gmail.com>
2026-07-23 22:17:02 -0400

    update meta description to English

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Blog Techwemate';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Discover the latest news, tutorials, and practical guides on tech, web development, and digital tools on Blog Techwemate.';
